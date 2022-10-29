638db9c5913b73b1c2b98b0bf824d457e952b75b
IvanNSBS <insbs@cin.ufpe.br>
2022-10-29 10:52:29 -0300

    Improving run backend scripts

--- create_backend_venv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf backend_server/.venv
-python -m venv backend_server/.venv
-source backend_server/.venv/Scripts/activate
-python -m pip install --upgrade pip
-pip install -r backend_server/src/requirements.txt
