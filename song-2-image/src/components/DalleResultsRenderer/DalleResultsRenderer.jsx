b72d48b906c4a98060b40d9809839076345450c7
Guilherme Melo <gacm@cin.ufpe.br>
2022-10-30 16:43:27 -0300

    implemented feature to select a dalle generated image

--- song-2-image/src/components/DalleResultsRenderer/DalleResultsRenderer.jsx
+++ song-2-image/src/components/DalleResultsRenderer/DalleResultsRenderer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ImagesContainer,
   ResultsContainer,
@@ -30,10 +30,20 @@ const SkeletonLoader = () => (
 );
 
 const DalleResultsRenderer = ({
-  isLoading = true,
+  isLoading = false,
   dalleResults = mockedData,
   strophe = mockedStrophe,
 }) => {
+  const [selectedImage, setSelectedImage] = useState(-1);
+
+  const handleClick = (index) => {
+    if (index == selectedImage) {
+      setSelectedImage(-1);
+    } else {
+      setSelectedImage(index);
+    }
+  };
+
   return (
     <>
       {isLoading ? (
@@ -41,13 +51,16 @@ const DalleResultsRenderer = ({
       ) : (
         <ResultsContainer>
           <ImagesContainer>
-            {dalleResults.map((result) => {
+            {dalleResults.map((result, index) => {
               return (
                 <StyledImage
                   src={result.generation.image_path}
                   key={result.id}
                   width={180}
                   height={180}
+                  index={index}
+                  selectedImage={selectedImage}
+                  onClick={() => handleClick(index)}
                 />
               );
             })}
@@ -69,7 +82,7 @@ const mockedData = [
     generation_type: "ImageGeneration",
     generation: {
       image_path:
-        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-90HVzHcQ7u9akdoS83Z9xVaQ/image.webp?st=2022-10-30T14%3A03%3A24Z&se=2022-10-30T16%3A01%3A24Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T14%3A10%3A08Z&ske=2022-11-06T14%3A10%3A08Z&sks=b&skv=2021-08-06&sig=BQSu%2BRxJ1OhYZNrdja19Y/3m9gyumCNpdgOzVsFgpvo%3D",
+        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-RsRMw20XYEhPqwxtWr8wnsHq/image.webp?st=2022-10-30T17%3A52%3A39Z&se=2022-10-30T19%3A50%3A39Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T18%3A49%3A27Z&ske=2022-11-06T18%3A49%3A27Z&sks=b&skv=2021-08-06&sig=NIlKhIvRVDbw1dc8oHynlDeQxuzX9cM4q1NkO%2BOIgn0%3D",
     },
     task_id: "task-5NnGRQwjePyZUzEEE0OsL8iD",
     prompt_id: "prompt-xsmVMCmyzOUWUTiOxPzVMKoG",
@@ -82,7 +95,7 @@ const mockedData = [
     generation_type: "ImageGeneration",
     generation: {
       image_path:
-        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-VNYaDDqImKn6UlneUiiWnvlG/image.webp?st=2022-10-30T14%3A03%3A24Z&se=2022-10-30T16%3A01%3A24Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T14%3A10%3A08Z&ske=2022-11-06T14%3A10%3A08Z&sks=b&skv=2021-08-06&sig=j/UFu0flmpGw9lgG86cplxgD6lveSHcSb%2BLIAkU%2B1gM%3D",
+        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-EWC5sjT0owkAZp7KGN3FlMiX/image.webp?st=2022-10-30T17%3A52%3A39Z&se=2022-10-30T19%3A50%3A39Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T18%3A49%3A27Z&ske=2022-11-06T18%3A49%3A27Z&sks=b&skv=2021-08-06&sig=JxLm9AZzBUQEU%2BBHJd2Mfg5CBuwJmbSPJqKZjSDNTSo%3D",
     },
     task_id: "task-5NnGRQwjePyZUzEEE0OsL8iD",
     prompt_id: "prompt-xsmVMCmyzOUWUTiOxPzVMKoG",
@@ -95,7 +108,7 @@ const mockedData = [
     generation_type: "ImageGeneration",
     generation: {
       image_path:
-        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-CEGcwqpevW8YMyikFK5MD5Cw/image.webp?st=2022-10-30T14%3A03%3A24Z&se=2022-10-30T16%3A01%3A24Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T14%3A10%3A08Z&ske=2022-11-06T14%3A10%3A08Z&sks=b&skv=2021-08-06&sig=7zm3R3GH/zmImxFZdT2S%2BBefUhO%2BP8jF8rhPChxOPRw%3D",
+        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-9TCqzNB4mgl1GUGOopDow5eh/image.webp?st=2022-10-30T17%3A52%3A39Z&se=2022-10-30T19%3A50%3A39Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T18%3A49%3A27Z&ske=2022-11-06T18%3A49%3A27Z&sks=b&skv=2021-08-06&sig=zpvzM88203y9mtm0J54vO8g50K/IE5PHHTQo%2BYZ5jug%3D",
     },
     task_id: "task-5NnGRQwjePyZUzEEE0OsL8iD",
     prompt_id: "prompt-xsmVMCmyzOUWUTiOxPzVMKoG",
@@ -108,7 +121,7 @@ const mockedData = [
     generation_type: "ImageGeneration",
     generation: {
       image_path:
-        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-T6PyuI7MMhFAlbfX0o8p3pBX/image.webp?st=2022-10-30T14%3A03%3A24Z&se=2022-10-30T16%3A01%3A24Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T14%3A10%3A08Z&ske=2022-11-06T14%3A10%3A08Z&sks=b&skv=2021-08-06&sig=htSqkAKjoelIlqr1ixStJVEsI2gzyWbq8K7ajwnvf%2BE%3D",
+        "https://openailabsprodscus.blob.core.windows.net/private/user-3WDSburElMKHvQlefMA7xz6u/generations/generation-bdVeA7O0HylMZcnqlzJsdjzi/image.webp?st=2022-10-30T17%3A52%3A39Z&se=2022-10-30T19%3A50%3A39Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-10-30T18%3A49%3A27Z&ske=2022-11-06T18%3A49%3A27Z&sks=b&skv=2021-08-06&sig=09uvVzvvdsOa7TqBeN6xRiAMCNxXKK2aS8WMZXFI9nc%3D",
     },
     task_id: "task-5NnGRQwjePyZUzEEE0OsL8iD",
     prompt_id: "prompt-xsmVMCmyzOUWUTiOxPzVMKoG",
--- song-2-image/src/components/DalleResultsRenderer/DalleResultsRenderer.styles.js
+++ song-2-image/src/components/DalleResultsRenderer/DalleResultsRenderer.styles.js
@@ -9,20 +9,40 @@ export const ImagesContainer = styled.div`
 `;
 
 export const StyledImage = styled(Image)`
-  border: 3px solid #333333;
+  border: ${(props) =>
+    props.selectedImage == props.index
+      ? "3px solid blue"
+      : props.selectedImage == -1
+      ? "3px solid #333333"
+      : "unset"};
   border-radius: 8px;
-  padding: 10px;
+  padding: ${(props) =>
+    props.selectedImage == props.index || props.selectedImage == -1
+      ? "10px"
+      : "0px"};
   cursor: pointer;
+  transform: ${(props) =>
+    props.selectedImage == props.index || props.selectedImage == -1
+      ? "scale(1)"
+      : "scale(0)"};
+  width: ${(props) =>
+    props.selectedImage == props.index || props.selectedImage == -1
+      ? "100%"
+      : "0px"};
+  transition: transform 0.2s 0s, width 0s 0s;
 
   &:hover {
-    border: 3px solid gray;
+    border: ${(props) =>
+      props.selectedImage == props.index ? "3px solid blue" : "3px solid gray"};
+    transform: scale(1.2);
+    padding: 1px;
   }
 `;
 
 export const ResultsContainer = styled.div`
   display: flex;
   flex-direction: row;
-
+  justify-content: center;
   gap: 32px;
 
   align-items: center;
